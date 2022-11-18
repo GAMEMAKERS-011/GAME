ed6e1e39e822ed7d209a7c954fdf8bb9a8d1f31a
JiayiGuo <1924534143@qq.com>
2022-11-18 23:39:37 +0800

    add transparency change

---
+++ DemoProject/Assets/MyAssets/Scripts/test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject character;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(character.transform.position, transform.position));
+        if (Vector3.Distance(character.transform.position, transform.position) < 10)
+        {
+            SpriteRenderer sp = GetComponent<SpriteRenderer>();
+            sp.color = new Color(1f, 1f, 1f, 0.5f);
+        }
+        else
+        {
+            SpriteRenderer sp = GetComponent<SpriteRenderer>();
+            sp.color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}
